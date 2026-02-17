004fd567ffb04a6d0b808e6d9b7efe291afae985
dev-ajijul-islam <devajijulislam@gmail.com>
2026-02-17 12:21:32 +0600

    onboarding screen refactored

--- lib/features/onboarding/controllers/onboarding_controller.dart
+++ lib/features/onboarding/controllers/onboarding_controller.dart
@@ -1,10 +1,32 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import '../../../constant/app_assets.dart';
 
 class OnboardingController extends GetxController {
   final PageController pageController = PageController();
 
   RxInt currentIndex = 0.obs;
+
+  List<Map<String, dynamic>> onBoardinList = [
+    {
+      "title": "Discover the world, one journey at a time.",
+      "subtitle":
+          "From hidden gems to iconic destinations, we make travel simple, inspiring, and unforgettable. Start your next adventure today.",
+      "image": AppAssets.onboard1,
+    },
+    {
+      "title": "Explore new horizons, one step at a time.",
+      "subtitle":
+          "Every trip holds a story waiting to be lived. Let us guide you to experience their charms, connect, and last a lifetime.",
+      "image": AppAssets.onboard2,
+    },
+    {
+      "title": "See the beauty, one journey at a time.",
+      "subtitle":
+          "Travel made simple and exciting—discover places you’ll love and moments you’ll never forget.",
+      "image": AppAssets.onboard3,
+    },
+  ];
 
   void onPageChanged(int index) {
     currentIndex.value = index;
--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:smart_travel_alearm/constant/app_assets.dart';
-import 'package:smart_travel_alearm/constant/app_color.dart';
 
+import '../../../../constant/app_color.dart';
 import '../../controllers/onboarding_controller.dart';
 import '../widgets/onboarding_page.dart';
 import '../widgets/onboarding_indicator.dart';
@@ -25,23 +24,14 @@ class OnboardingScreen extends StatelessWidget {
                 child: PageView(
                   controller: controller.pageController,
                   onPageChanged: controller.onPageChanged,
-                  children: const [
-                    OnboardingPage(
-                      image: AppAssets.onboard1,
-                      title: "Discover the world, one journey at a time.",
-                      subtitle: "From hidden gems to iconic destinations, we make travel simple, inspiring, and unforgettable. Start your next adventure today.",
-                    ),
-                    OnboardingPage(
-                      image: AppAssets.onboard2,
-                      title: "Explore new horizons, one step at a time.",
-                      subtitle: "Every trip holds a story waiting to be lived. Let us guide you to experience their charms, connect, and last a lifetime.",
-                    ),
-                    OnboardingPage(
-                      image: AppAssets.onboard3,
-                      title: "See the beauty, one journey at a time.",
-                      subtitle: "Travel made simple and exciting—discover places you’ll love and moments you’ll never forget.",
-                    ),
-                  ],
+                  children: List.generate(controller.onBoardinList.length, (index) {
+                    final Map<String,dynamic> data = controller.onBoardinList[index];
+                    return   OnboardingPage(
+                      image: data["image"],
+                      title: data["title"],
+                      subtitle: data["subtitle"],
+                    );
+                  },),
                 ),
               ),
 
