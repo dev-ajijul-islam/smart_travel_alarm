0d50e314a67563feec26de981c6715effaa11273
dev-ajijul-islam <devajijulislam@gmail.com>
2026-02-17 12:00:48 +0600

    assets are moved to AppAssets

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:smart_travel_alearm/constant/app_assets.dart';
 import 'package:smart_travel_alearm/constant/app_color.dart';
+
 import '../../controllers/onboarding_controller.dart';
 import '../widgets/onboarding_page.dart';
 import '../widgets/onboarding_indicator.dart';
@@ -25,17 +27,17 @@ class OnboardingScreen extends StatelessWidget {
                   onPageChanged: controller.onPageChanged,
                   children: const [
                     OnboardingPage(
-                      image: "assets/images/onboard1.png",
+                      image: AppAssets.onboard1,
                       title: "Discover the world, one journey at a time.",
                       subtitle: "From hidden gems to iconic destinations, we make travel simple, inspiring, and unforgettable. Start your next adventure today.",
                     ),
                     OnboardingPage(
-                      image: "assets/images/onboard2.png",
+                      image: AppAssets.onboard2,
                       title: "Explore new horizons, one step at a time.",
                       subtitle: "Every trip holds a story waiting to be lived. Let us guide you to experience their charms, connect, and last a lifetime.",
                     ),
                     OnboardingPage(
-                      image: "assets/images/onboard3.png",
+                      image: AppAssets.onboard3,
                       title: "See the beauty, one journey at a time.",
                       subtitle: "Travel made simple and exciting—discover places you’ll love and moments you’ll never forget.",
                     ),
